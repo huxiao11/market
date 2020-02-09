61fb139a535e4434144e1d7e3832a0f187c563d1
三娘家的扈筱 <sanniangjiadehuxiao@sannianiaodeair.home>
2020-02-09 11:31:31 +0800

    轮播图swiper提交

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="home">
       <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar> 
+      <home-swiper :banners="banners"/>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper'
+
 import {getHomeMultidata} from 'network/home'
 
 export default {
     components: {
-      NavBar
+      NavBar,
+      HomeSwiper
     },
     data() {
       return {
